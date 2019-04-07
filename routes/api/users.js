a7c8c90f59ed37f737d88e497b540b169878d150
hemanthgowdaa <hemanthgowda9945@gmail.com>
2019-04-07 19:35:53 +0530

    second commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
 
 router.get('/test',(req,res)=>res.json({ msg :"message from user"}));
+
+router.post('/register',(req,res)=>{
+User.findOne({email:req.body.email})
+.then(user =>{
+if(user){
+  return res.status(400).json({email:'email alredyy exist'})
+}else{
+  const newUser = new User ({
+  
+    name : req.body.name,
+    email : req.body.name,
+    avatar,
+    password :req.body.password
+  });
+}
+
+})
+
+});
 
 module.exports = router ;
